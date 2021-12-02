ae79d843bf6aae83e2fbc38c91005c017916f918
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-02 09:14:48 -0500

    Update Main.hx

--- src/main/haxe/keystone_games/eightflights/Main.hx
+++ src/main/haxe/keystone_games/eightflights/Main.hx
@@ -9,8 +9,22 @@ class Main extends Sprite
 	public override function new()
 	{
 		super();
+		preInit();
+	}
+	
+	public function preInit() {
+		init();
+	}
+	
+	public function init() {
 		Lib.current.addChild(new FlxGame(Reference.flxGameArgs.gameWidth, Reference.flxGameArgs.gameHeight, Reference.flxGameArgs.initialState,
 			Reference.flxGameArgs.zoom, Reference.flxGameArgs.framerate, Reference.flxGameArgs.framerate, Reference.flxGameArgs.skipSplash,
 			Reference.flxGameArgs.startFullscreen));
+		
+		postInit();
+	}
+	
+	public function postInit() {
+	
 	}
 }
